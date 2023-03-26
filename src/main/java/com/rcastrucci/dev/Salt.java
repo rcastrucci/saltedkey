480f91d5109bdd04e3c28f13a5e6f92451986b11
Renne Castrucci <rennecastrucci@gmail.com>
2023-03-26 16:40:18 -0300

    Created a Maven project

--- src/main/java/com/rcastrucci/dev/Salt.java
+++ src/main/java/com/rcastrucci/dev/Salt.java
@@ -1,3 +1,5 @@
+package com.rcastrucci.dev;
+
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.util.ArrayList;
@@ -5,8 +7,8 @@ import java.util.Arrays;
 import java.util.Date;
 
 /**
+ * Simple repository to create and verify salted keys.
  * @author dev.rcastrucci
- * github: <a href="https://github.com/rcastrucci"> Github </a>
  */
 public class Salt {
 
@@ -18,10 +20,21 @@ public class Salt {
         this.spice = generateSpices().get(1);
     }
 
+    /**
+     * Generate a public key out of your private key this public key will be valid for about 1.5 minutes
+     * @param privateKey your personal private key
+     * @return a string format with a public key
+     */
     public String createSaltedKey(String privateKey) {
         return hash(privateKey+salt);
     }
 
+    /**
+     * Verify if the public key match with a private key
+     * @param publicKey a public key
+     * @param privateKey a private key
+     * @return boolean true if they match otherwise false
+     */
     public boolean verifySaltedKey(String publicKey, String privateKey) {
         return (hash(privateKey+salt).equals(publicKey) || hash(privateKey+spice).equals(publicKey));
     }
@@ -46,8 +59,8 @@ public class Salt {
     private ArrayList<String> generateSpices() {
         Long ts = new Date().getTime();
         return new ArrayList<>(Arrays.asList(
-            String.valueOf(ts).substring(0, String.valueOf(ts).length() - 5),
-            String.valueOf(ts+100000).substring(0, String.valueOf(ts+100000).length() - 5)
+                String.valueOf(ts).substring(0, String.valueOf(ts).length() - 5),
+                String.valueOf(ts+100000).substring(0, String.valueOf(ts+100000).length() - 5)
         ));
     }
 
